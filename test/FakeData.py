10378c228dc44366ee2ebc626e3831c3f3666aa5
srgzyq_air <srgzyq@playcrab.com>
2016-05-05 19:25:39 +0800

    unittest

---
+++ test/FakeData.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append("..")
+from food.readXlsInfo import ReadXlsInfo
+
+xlsInfoOne = ReadXlsInfo("./xlsOne.xls")
+
+xlsInfoTwo = ReadXlsInfo("./xlsTwo.xls")
+
+xlsOne = {
+    "user":
+    {
+        "title": ["date", "people", "income","company"],
+        "content":
+        [["2016/4/25", 4, 300], ["2016/4/25", 2, 215], ["2016/4/25", 4, 300]]
+    },
+    "member":
+    {
+        "title": ["date", "num", "income"],
+        "content":
+        [["2016/4/25", 1, 20], ["2016/4/25", 2, 40]]
+    }
+}
+
+
+xlsTow = {
+    "user":
+    {
+        "title": ["date", "people", "income","company"],
+        "content":
+        [["2016/4/26", 1, 12], ["2016/4/26", 8, 600], ["2016/4/26", 2, 180]]
+    },
+    "member":
+    {
+        "title": ["date", "num", "income"],
+        "content":
+        [["2016/4/26", 4, 80]]
+    }
+}
---
+++ test/combXlsData_test.py
@@ -0,0 +1,45 @@
+import unittest
+import FakeData
+
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append("..")
+
+from food.combXlsData import CombXlsData
+
+
+class TestCombXlsData(unittest.TestCase):
+
+    def setUp(self):
+        self.comb = CombXlsData()
+        self.comb.setXlsData(FakeData.xlsInfoOne)
+        self.comb.setXlsData(FakeData.xlsInfoTwo)
+        self.comb.initTableInfo()
+        self.comb.initTableTitleInfo()
+        self.comb.mergeTableContent()
+        self.result = self.comb.getResultData()
+        # print "setUp.."
+
+    def tearDown(self):
+        # print "tearDown.."
+        pass
+
+    def test_setXlsData(self):
+        self.assertEquals(len(self.comb.doMergeData), 2)
+
+    def test_initTableInfo(self):
+        self.assertTrue(u'income' in self.result)
+        self.assertTrue(u'member' in self.result)
+
+    def test_initTableTitleInfo(self):
+        self.assertTrue('title' in self.result[u'income'])
+        self.assertTrue('title' in self.result[u'member'])
+        self.assertTrue('company'in self.result[u'income'][u'title'])
+        self.assertTrue('num'in self.result[u'member'][u'title'])
+
+    def test_mergeTableContent(self):
+        self.assertEquals(len(self.result['income']['content']), 14)
+        self.assertEquals(len(self.result['member']['content']), 6)
+
+if __name__ == '__main__':
+    unittest.main()
